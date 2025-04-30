98636f22ee523c86023f2c6e1023da2f3d9a02e9
YvanMazy <mazy.yvan@protonmail.com>
2025-04-30 11:51:28 +0200

    :sparkles: Add a cache layer source implementation

---
+++ common/src/main/java/be/yvanmazy/proxyonlinelinker/common/status/source/CacheLayerSource.java
@@ -0,0 +1,35 @@
+package be.yvanmazy.proxyonlinelinker.common.status.source;
+
+import be.yvanmazy.proxyonlinelinker.common.util.Preconditions;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Objects;
+
+public class CacheLayerSource implements StatusSource {
+
+    private final StatusSource delegate;
+    private final long expirationDelay;
+
+    private int lastFetched;
+    private long lastUpdate;
+
+    public CacheLayerSource(final @NotNull StatusSource delegate, final @Range(from = 1L, to = Long.MAX_VALUE) long expirationDelay) {
+        this.delegate = Objects.requireNonNull(delegate, "delegate must not be null");
+        Preconditions.checkRange(expirationDelay, 1L, Long.MAX_VALUE, "expirationDelay");
+        this.expirationDelay = expirationDelay;
+        // TODO: Add an option to don't cache failures
+    }
+
+    @Override
+    public int fetch() {
+        final long now = System.currentTimeMillis();
+        if (now - this.lastUpdate < this.expirationDelay) {
+            return this.lastFetched;
+        }
+        this.lastFetched = this.delegate.fetch();
+        this.lastUpdate = now;
+        return this.lastFetched;
+    }
+
+}
--- common/src/main/java/be/yvanmazy/proxyonlinelinker/common/status/source/StatusSource.java
+++ common/src/main/java/be/yvanmazy/proxyonlinelinker/common/status/source/StatusSource.java
@@ -2,7 +2,6 @@ package be.yvanmazy.proxyonlinelinker.common.status.source;
 
 import org.jetbrains.annotations.CheckReturnValue;
 
-// TODO: Add cache layer source
 public interface StatusSource {
 
     @CheckReturnValue
