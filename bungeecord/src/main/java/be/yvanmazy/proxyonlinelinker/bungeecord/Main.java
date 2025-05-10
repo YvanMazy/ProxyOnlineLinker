a1ce371f683ae35f27f4834d8887353ce711a105
YvanMazy <mazy.yvan@protonmail.com>
2025-05-10 20:05:09 +0200

    :recycle: Update BungeeCord implementation to use the common part

--- bungeecord/src/main/java/be/yvanmazy/proxyonlinelinker/bungeecord/Main.java
+++ bungeecord/src/main/java/be/yvanmazy/proxyonlinelinker/bungeecord/Main.java
@@ -1,60 +1,34 @@
 package be.yvanmazy.proxyonlinelinker.bungeecord;
 
 import be.yvanmazy.proxyonlinelinker.bungeecord.listener.DelegateStatusListener;
-import be.yvanmazy.proxyonlinelinker.common.config.Configuration;
-import be.yvanmazy.proxyonlinelinker.common.config.ConfigurationReader;
-import be.yvanmazy.proxyonlinelinker.common.status.DefaultOnlineManager;
+import be.yvanmazy.proxyonlinelinker.common.ProxyOnlineLinker;
 import be.yvanmazy.proxyonlinelinker.common.status.OnlineManager;
 import be.yvanmazy.proxyonlinelinker.common.status.replacement.ReplacementStrategy;
 import net.md_5.bungee.api.plugin.Plugin;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.logging.Level;
-
 public final class Main extends Plugin {
 
-    private Configuration configuration;
-
-    private OnlineManager onlineManager;
+    private ProxyOnlineLinker proxyOnlineLinker;
 
     @Override
     public void onEnable() {
-        final Path path = this.getDataFolder().toPath().resolve("config.yml");
-        try {
-            this.configuration = ConfigurationReader.read(path);
-        } catch (final IOException exception) {
-            this.getLogger().log(Level.SEVERE, "Failed to read configuration", exception);
-            return;
-        }
-
-        if (this.configuration.status().enabled()) {
-            this.initStatusHandling();
-        }
+        this.proxyOnlineLinker = new ProxyOnlineLinker("BungeeCord", () -> this.getProxy().getOnlineCount(), this::initReplacement);
+        this.proxyOnlineLinker.onEnable(this.getDataFolder().toPath().resolve("config.yml"));
     }
 
     @Override
     public void onDisable() {
-        if (this.onlineManager != null) {
-            this.onlineManager.stop();
+        if (this.proxyOnlineLinker != null) {
+            this.proxyOnlineLinker.onDisable();
         }
     }
 
-    private void initStatusHandling() {
-        this.onlineManager = new DefaultOnlineManager();
-        this.onlineManager.start(this.configuration);
-
-        final Configuration.Status.Replacement replacement = this.configuration.status().replacement();
-
-        if (replacement.strategy() == ReplacementStrategy.NONE) {
+    private void initReplacement(final OnlineManager onlineManager, final ReplacementStrategy replacementStrategy) {
+        if (replacementStrategy == ReplacementStrategy.DELEGATE) {
+            this.getProxy().getPluginManager().registerListener(this, new DelegateStatusListener(onlineManager));
             return;
         }
-
-        if (replacement.strategy() == ReplacementStrategy.DELEGATE) {
-            this.getProxy().getPluginManager().registerListener(this, new DelegateStatusListener(this.onlineManager));
-        } else {
-            throw new UnsupportedOperationException("Replacement strategy " + replacement.strategy() + " is not supported");
-        }
+        throw new UnsupportedOperationException("Replacement strategy " + replacementStrategy + " is not supported");
     }
 
 }
