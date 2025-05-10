67bbdff6d912cefb3b3db642b657a83ee4241a44
YvanMazy <mazy.yvan@protonmail.com>
2025-05-10 19:59:12 +0200

    :sparkles: Add a draft implementation of Spigot

--- spigot/src/main/java/be/yvanmazy/proxyonlinelinker/spigot/Main.java
+++ spigot/src/main/java/be/yvanmazy/proxyonlinelinker/spigot/Main.java
@@ -1,12 +1,30 @@
 package be.yvanmazy.proxyonlinelinker.spigot;
 
+import be.yvanmazy.proxyonlinelinker.common.ProxyOnlineLinker;
+import be.yvanmazy.proxyonlinelinker.common.status.OnlineManager;
+import be.yvanmazy.proxyonlinelinker.common.status.replacement.ReplacementStrategy;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private ProxyOnlineLinker proxyOnlineLinker;
+
     @Override
     public void onEnable() {
-        // TODO: Implement Spigot support
+        this.proxyOnlineLinker = new ProxyOnlineLinker("Spigot", Bukkit.getOnlinePlayers()::size, this::initReplacement);
+        this.proxyOnlineLinker.onEnable(this.getDataFolder().toPath().resolve("config.yml"));
+    }
+
+    @Override
+    public void onDisable() {
+        if (this.proxyOnlineLinker != null) {
+            this.proxyOnlineLinker.onDisable();
+        }
+    }
+
+    private void initReplacement(final OnlineManager onlineManager, final ReplacementStrategy replacementStrategy) {
+        throw new UnsupportedOperationException("Replacement strategies is currently not supported");
     }
 
 }
