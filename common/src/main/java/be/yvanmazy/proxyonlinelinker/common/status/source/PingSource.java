f43d981a7882fa29da94930cd6d09e78f742a328
YvanMazy <mazy.yvan@protonmail.com>
2025-05-11 10:43:13 +0200

    :page_facing_up: Add MIT license

--- common/src/main/java/be/yvanmazy/proxyonlinelinker/common/status/source/PingSource.java
+++ common/src/main/java/be/yvanmazy/proxyonlinelinker/common/status/source/PingSource.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Yvan Mazy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package be.yvanmazy.proxyonlinelinker.common.status.source;
 
 import be.yvanmazy.proxyonlinelinker.common.util.Preconditions;
@@ -92,6 +116,7 @@ public class PingSource implements StatusSource {
     private byte[] buildHandshakePacket() throws IOException {
         final ByteArrayOutputStream stream = new ByteArrayOutputStream();
         writeVarInt(stream, 0x00); // Packet ID
+        // TODO: Add an option to configure protocol
         writeVarInt(stream, 759); // Protocol ID (759 = 1.20.4)
         writeString(stream, this.host); // Write host
         stream.write((this.port >>> 8) & 0xFF); // Port high‑byte
